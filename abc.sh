4c0bb18b82d5966d24b6a47444dffe241de2dc79
esteemz <Drezilad@gmail.com>
2023-01-24 16:09:27 -0700

    Create abc.sh

---
+++ abc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat "$0" > abc.txt
+
+#Outputs the contents of this bash script into a txt file named abc.txt in order to be referenced later
+
+#can add a dir to output the txt file to if desired.
+
+#Perms: chmod +x abc.sh
